5c389caadd7bbad0bcd9719c075e9950e21a98e1
seanard1 <seanard@gmail.com>
2023-07-01 17:38:24 -0400

    Updated ReadMe file

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,3 +1,4 @@
+-- Question 01
 -- List the employee number, last name, first name, sex, and salary of each employee.
 
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
@@ -5,14 +6,16 @@ FROM employees AS e
 INNER JOIN salaries AS s
 ON e.emp_no = s.emp_no;
 
+-- Question 02
 -- List the first name, last name, and hire date for the employees who were hired in 1986.
 -- https://www.rudderstack.com/guides/queries-casting-postgresql/#:~:text=The%20TO_DATE%20function%20in%20PostgreSQL,to_timestamp(text%2C%20text)%20.
 -- https://www.postgresqltutorial.com/postgresql-date-functions/postgresql-date_part/
 
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE DATE_PART('YEAR', TO_DATE(hire_date, 'MM/DD/YYY')) = 1986
+WHERE DATE_PART('YEAR', TO_DATE(hire_date, 'MM/DD/YYY')) = 1986;
 
+-- Question 03
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name.
 
 SELECT d.dept_no, n.dept_name, d.emp_no, e.last_name, e.first_name
@@ -20,9 +23,9 @@ FROM dept_manager AS d
 INNER JOIN departments AS n
 ON d.dept_no = n.dept_no
 INNER JOIN employees AS e
-ON d.emp_no = e.emp_no
+ON d.emp_no = e.emp_no;
 
-
+-- Question 04
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
 
 SELECT n.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
@@ -30,15 +33,17 @@ FROM employees AS e
 INNER JOIN dept_emp AS n
 ON n.emp_no = e.emp_no
 INNER JOIN departments AS d
-ON n.dept_no = d.dept_no
+ON n.dept_no = d.dept_no;
 
+-- Question 05
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules'
-AND last_name LIKE 'B%'
+AND last_name LIKE 'B%';
 
+-- Question 06
 -- List each employee in the Sales department, including their employee number, last name, and first name.
 
 SELECT emp_no, last_name, first_name
@@ -51,8 +56,9 @@ WHERE emp_no IN (
 		FROM departments
 		WHERE dept_name = 'Sales'
 	)
-)
+);
 
+-- Question 07
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
@@ -61,8 +67,9 @@ INNER JOIN dept_emp AS n
 ON n.emp_no = e.emp_no
 INNER JOIN departments AS d
 ON n.dept_no = d.dept_no
-WHERE d.dept_name = 'Sales' OR dept_name = 'Development'
+WHERE d.dept_name = 'Sales' OR dept_name = 'Development';
 
+-- Question 08
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 
 SELECT last_name, COUNT(last_name)
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # sql-challenge
+
+## Introduction/Premise
+
+It’s been two weeks since you were hired as a new data engineer at Pewlett Hackard (a fictional company). Your first major task is to do a research project about people whom the company employed during the 1980s and 1990s. All that remains of the employee database from that period are six CSV files.
+
+For this project, you’ll design the tables to hold the data from the CSV files, import the CSV files into a SQL database, and then answer questions about the data. That is, you’ll perform data modeling, data engineering, and data analysis, respectively.
+
+## Summary
+
+This repository contains the outputs for the modelling, engineering and analysis of the EmployeeSQL csv files. 
+
+Modelling: ERD diagram (ERD.jpg) generated with QuickDatabaseDiagrams.com shows the relationship between the tables and sets up the SQL table creation
+
+Engineering: Tables schema (tables_schema.sql) includes the queries for setup of the tables in pgAdmin, where they can then be imported.
+
+Analysis: The analysis queries (queries.sql) includes the answers to the provided questions, with associated screenshots of results in (images) folder as Question01.jpg, Question02.jpg, etc..
+
+## Citations
+
+-- https://www.rudderstack.com/guides/queries-casting-postgresql/#:~:text=The%20TO_DATE%20function%20in%20PostgreSQL,to_timestamp(text%2C%20text)%20.
+-- https://www.postgresqltutorial.com/postgresql-date-functions/postgresql-date_part/
+
+I struggled a fair bit with the date formatting. At first, attempts to import the csv were failing because I tried to make the birth_date and hire_date as DATE variables in pgAdmin. This was failing because the csv file contained dates that were not in postgresql's default ISO format. I tried to change the DATESTYLE in pgAdmin, but was unable to successfully import the file.
+
+These failures led me to import the dates as VARCHAR and then parse them as dates afterwards. This was done using the TO_DATE and DATE_PART commands I found at the links above. 
