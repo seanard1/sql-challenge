b8547dca355d80dd76bf6717458f07acc2318f40
seanard1 <seanard@gmail.com>
2023-07-01 17:46:21 -0400

    Fixed ERD diagram

--- EmployeeSQL/tables_schema.sql
+++ EmployeeSQL/tables_schema.sql
@@ -6,11 +6,11 @@ CREATE TABLE titles (
 CREATE TABLE employees (
     emp_no INT PRIMARY KEY,
     emp_title_id VARCHAR(10) NOT NULL,
-    birth_date VARCHAR(35) NOT NULL,
+    birth_date VARCHAR(12) NOT NULL,
     first_name VARCHAR(35) NOT NULL,
     last_name VARCHAR(35) NOT NULL,
     sex VARCHAR(1) NOT NULL,
-    hire_date VARCHAR(35) NOT NULL,
+    hire_date VARCHAR(12) NOT NULL,
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
