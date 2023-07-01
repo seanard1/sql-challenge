c59722f25254d99633824acf47097921af7be2d0
seanard1 <seanard@gmail.com>
2023-07-01 14:41:30 -0400

    Added NOT NULL to logical columns

--- EmployeeSQL/tables_schema.sql
+++ EmployeeSQL/tables_schema.sql
@@ -1,40 +1,40 @@
 CREATE TABLE titles (
     title_id VARCHAR(5) PRIMARY KEY,
-    title VARCHAR(30)
+    title VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE employees (
     emp_no INT PRIMARY KEY,
-    emp_title_id VARCHAR(10),
-    birth_date VARCHAR(35),
-    first_name VARCHAR(35),
-    last_name VARCHAR(35),
-    sex VARCHAR(1),
-    hire_date VARCHAR(35),
+    emp_title_id VARCHAR(10) NOT NULL,
+    birth_date VARCHAR(35) NOT NULL,
+    first_name VARCHAR(35) NOT NULL,
+    last_name VARCHAR(35) NOT NULL,
+    sex VARCHAR(1) NOT NULL,
+    hire_date VARCHAR(35) NOT NULL,
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 CREATE TABLE departments (
     dept_no VARCHAR(4) PRIMARY KEY,
-    dept_name VARCHAR(30)
+    dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE salaries (
-    emp_no INTEGER,
-    salary INTEGER,
+    emp_no INTEGER NOT NULL,
+    salary INTEGER NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 
 CREATE TABLE dept_emp (
-    emp_no INT,
+    emp_no INT NOT NULL,
     dept_no VARCHAR(4),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager (
-    dept_no VARCHAR(4),
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INT,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
